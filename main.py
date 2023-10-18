854e793884c2ba24f895be4920ddbb8ecc0513a5
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-10-18 15:23:11 -0500

    CNN implemented, training is done

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import cv2
 import os
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 def load_pos(train_file):
+    print("Cargando anotaciones y muestras positivas...")
     annotations = []
+    positive_samples = []
     with open(train_file, 'r') as file:
         for line in file:
             parts = line.strip().split(' ')
@@ -14,23 +18,29 @@ def load_pos(train_file):
                 x_max = int(parts[4])
                 y_max = int(parts[5])
                 annotations.append((image_path, x_min, y_min, x_max, y_max))
-    return annotations
+                image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+                if image is not None:
+                    roi = image[y_min:y_max, x_min:x_max]
+                    positive_samples.append(roi)
+    print("Anotaciones y muestras positivas cargadas.")
+    return annotations, positive_samples
 
-def create_ps( annotations):
+def create_ps(positive_samples, target_size):
+    print("Redimensionando muestras positivas...")
     samples = []
-    for annotation in annotations:
-        image_path, x_min, y_min, x_max, y_max = annotation
-        image = cv2.imread(os.path.join(image_path), cv2.IMREAD_GRAYSCALE)
-        if image is not None:
-            roi = image[y_min:y_max, x_min:x_max]
-            samples.append(roi)
+    for sample in positive_samples:
+        # Redimensionar la imagen al tamaño deseado
+        resized_sample = cv2.resize(sample, target_size)  # target_size debe ser una tupla (width, height)
+        samples.append(resized_sample)
+    print("Muestras positivas redimensionadas.")
     return samples
 
-def train(positive_samples, negative_images_dir, model_filename):
+def train_cnn(positive_samples, negative_images_dir, model_filename, input_shape, num_classes, epochs):
+    print("Entrenando la red neuronal convolucional...")
     # Leer imágenes negativas
     negative_images = [os.path.join(negative_images_dir, img) for img in os.listdir(negative_images_dir)]
 
-    # Crear listas para almacenar las rutas de las imágenes y etiquetas
+    # Crear listas para almacenar las imágenes y etiquetas
     images = positive_samples
     labels = [1] * len(positive_samples)  # Etiqueta 1 para imágenes positivas
 
@@ -40,44 +50,42 @@ def train(positive_samples, negative_images_dir, model_filename):
         if image is not None:
             # Escalar la imagen al rango [0, 1] y convertirla a CV_32F
             image = cv2.normalize(image.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
-            images.append(image)
+            # Redimensionar la imagen al tamaño deseado
+            resized_image = cv2.resize(image, input_shape[:2])  # target_size debe ser una tupla (width, height)
+            images.append(resized_image)
             labels.append(0)  # Etiqueta 0 para imágenes negativas
 
-    # Crear un detector HOG
-    hog = cv2.HOGDescriptor()
-    svm = cv2.ml.SVM_create()
+    # Convertir las listas a matrices numpy
+    images = np.array(images)
+    labels = np.array(labels)
 
-    # Entrenar el detector
-    samples = np.array(images, dtype=np.float32)  # Cambiar el tipo de dato a CV_32F
-    responses = np.array(labels, dtype=np.int32)  # Mantener las etiquetas como CV_32S
-    svm.setType(cv2.ml.SVM_C_SVC)
-    svm.setKernel(cv2.ml.SVM_LINEAR)
-    svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
-    samples = samples.reshape(-1, np.prod(samples.shape[1:])) 
-    svm.train(samples, cv2.ml.ROW_SAMPLE, responses)
-    svm.save(model_filename)
+    # Normalizar los valores de píxeles al rango [0, 1]
+    images = images / 255.0
 
-def resize_images(input_dir, output_dir, target_size):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    valid_extensions = ['.jpg', '.jpeg', '.png']  # Extensiones válidas de imágenes
-    
-    for filename in os.listdir(input_dir):
-        if any(filename.lower().endswith(ext) for ext in valid_extensions):
-            input_path = os.path.join(input_dir, filename)
-            output_path = os.path.join(output_dir, filename)
-            # Cargar la imagen
-            image = cv2.imread(input_path)
-            
-            if image is not None:
-                # Redimensionar la imagen al tamaño deseado
-                resized_image = cv2.resize(image, target_size)  # target_size debe ser una tupla (width, height)
-                # Guardar la imagen redimensionada
-                cv2.imwrite(output_path, resized_image)
-                print(f"Imagen redimensionada y guardada: {output_path}")
+    # Expandir las dimensiones de las imágenes para que sean tridimensionales
+    images = np.expand_dims(images, axis=-1)  # Añade una dimensión de canales
+
+    # Crear el modelo CNN
+    model = keras.Sequential([
+        keras.layers.Input(shape=input_shape),
+        keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(num_classes, activation='softmax')
+    ])
+
+    # Compilar el modelo
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Entrenar el modelo
+    model.fit(images, labels, epochs=epochs)
+
+    # Guardar el modelo
+    model.save(model_filename)
+    print(f"Modelo entrenado y guardado en {model_filename}.")
 
 def main():
-    
     print('Identificación de granos de café maduros')
     # Directorios para imágenes positivas y negativas
     positive_images_dir = "./DB"
@@ -89,15 +97,16 @@ def main():
         print(f"El archivo de anotaciones '{ent_file}' no se encontró. Asegúrate de que esté en la misma ubicación que tus imágenes positivas.")
         return
 
-    annotations = load_pos(ent_file)
-    positive_samples = create_ps(annotations)
-    negative_resized_dir = "./N_DB_resized"
-    target_size = (64, 64)
-    resize_images(negative_images_dir, negative_resized_dir, target_size)
-    model_filename = "detector.yml"
+    annotations, positive_samples = load_pos(ent_file)
+    target_size = (128, 128)  # Tamaño deseado para las imágenes
+    num_classes = 2  # 2 clases: positiva y negativa
+    epochs = 10  # Número de épocas de entrenamiento
+
+    model_filename = "cnn_model.h5"
     if not os.path.isfile(model_filename):
-        train(positive_samples, negative_resized_dir, model_filename)
-        print(f"Modelo entrenado y guardado en {model_filename}")
+        positive_samples = create_ps(positive_samples, target_size)
+        input_shape = (target_size[0], target_size[1], 1)  # Tamaño de entrada de la CNN (agregando una dimensión de canal)
+        train_cnn(positive_samples, negative_images_dir, model_filename, input_shape, num_classes, epochs)
 
 if __name__ == '__main__':
     main()
