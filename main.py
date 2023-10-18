d3c81b36bd182057a0b696b3dc735040fbf4602b
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2023-10-18 15:31:55 -0500

    CNN implemented, training is done

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
+
 def load_pos(train_file):
     print("Cargando anotaciones y muestras positivas...")
     annotations = []
     positive_samples = []
-    with open(train_file, 'r') as file:
+    with open(train_file, "r") as file:
         for line in file:
-            parts = line.strip().split(' ')
+            parts = line.strip().split(" ")
             if len(parts) >= 6:
                 image_path = parts[0]
                 x_min = int(parts[2])
@@ -25,20 +26,35 @@ def load_pos(train_file):
     print("Anotaciones y muestras positivas cargadas.")
     return annotations, positive_samples
 
+
 def create_ps(positive_samples, target_size):
     print("Redimensionando muestras positivas...")
     samples = []
     for sample in positive_samples:
         # Redimensionar la imagen al tamaño deseado
-        resized_sample = cv2.resize(sample, target_size)  # target_size debe ser una tupla (width, height)
+        resized_sample = cv2.resize(
+            sample, target_size
+        )  # target_size debe ser una tupla (width, height)
         samples.append(resized_sample)
     print("Muestras positivas redimensionadas.")
     return samples
 
-def train_cnn(positive_samples, negative_images_dir, model_filename, input_shape, num_classes, epochs):
+
+def train_cnn(
+    positive_samples,
+    negative_images_dir,
+    model_filename,
+    input_shape,
+    num_classes,
+    epochs,
+    model_saving_path,
+):
     print("Entrenando la red neuronal convolucional...")
     # Leer imágenes negativas
-    negative_images = [os.path.join(negative_images_dir, img) for img in os.listdir(negative_images_dir)]
+    negative_images = [
+        os.path.join(negative_images_dir, img)
+        for img in os.listdir(negative_images_dir)
+    ]
 
     # Crear listas para almacenar las imágenes y etiquetas
     images = positive_samples
@@ -49,9 +65,13 @@ def train_cnn(positive_samples, negative_images_dir, model_filename, input_shape
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         if image is not None:
             # Escalar la imagen al rango [0, 1] y convertirla a CV_32F
-            image = cv2.normalize(image.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
+            image = cv2.normalize(
+                image.astype("float"), None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F
+            )
             # Redimensionar la imagen al tamaño deseado
-            resized_image = cv2.resize(image, input_shape[:2])  # target_size debe ser una tupla (width, height)
+            resized_image = cv2.resize(
+                image, input_shape[:2]
+            )  # target_size debe ser una tupla (width, height)
             images.append(resized_image)
             labels.append(0)  # Etiqueta 0 para imágenes negativas
 
@@ -66,35 +86,43 @@ def train_cnn(positive_samples, negative_images_dir, model_filename, input_shape
     images = np.expand_dims(images, axis=-1)  # Añade una dimensión de canales
 
     # Crear el modelo CNN
-    model = keras.Sequential([
-        keras.layers.Input(shape=input_shape),
-        keras.layers.Conv2D(32, (3, 3), activation='relu'),
-        keras.layers.MaxPooling2D((2, 2)),
-        keras.layers.Flatten(),
-        keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dense(num_classes, activation='softmax')
-    ])
+    model = keras.Sequential(
+        [
+            keras.layers.Input(shape=input_shape),
+            keras.layers.Conv2D(32, (3, 3), activation="relu"),
+            keras.layers.MaxPooling2D((2, 2)),
+            keras.layers.Flatten(),
+            keras.layers.Dense(128, activation="relu"),
+            keras.layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
 
     # Compilar el modelo
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
 
     # Entrenar el modelo
     model.fit(images, labels, epochs=epochs)
 
     # Guardar el modelo
-    model.save(model_filename)
+    model.save(f"{model_saving_path}/{model_filename}")
     print(f"Modelo entrenado y guardado en {model_filename}.")
 
+
 def main():
-    print('Identificación de granos de café maduros')
+    print("Identificación de granos de café maduros")
     # Directorios para imágenes positivas y negativas
     positive_images_dir = "./DB"
     negative_images_dir = "./N_DB"
+    model_saving_path = "./model_training"
     # Ruta al archivo de anotaciones
     ent_file = "./DB/datos_entrenamiento.txt"
 
     if not os.path.isfile(ent_file):
-        print(f"El archivo de anotaciones '{ent_file}' no se encontró. Asegúrate de que esté en la misma ubicación que tus imágenes positivas.")
+        print(
+            f"El archivo de anotaciones '{ent_file}' no se encontró. Asegúrate de que esté en la misma ubicación que tus imágenes positivas."
+        )
         return
 
     annotations, positive_samples = load_pos(ent_file)
@@ -105,8 +133,21 @@ def main():
     model_filename = "cnn_model.h5"
     if not os.path.isfile(model_filename):
         positive_samples = create_ps(positive_samples, target_size)
-        input_shape = (target_size[0], target_size[1], 1)  # Tamaño de entrada de la CNN (agregando una dimensión de canal)
-        train_cnn(positive_samples, negative_images_dir, model_filename, input_shape, num_classes, epochs)
+        input_shape = (
+            target_size[0],
+            target_size[1],
+            1,
+        )  # Tamaño de entrada de la CNN (agregando una dimensión de canal)
+        train_cnn(
+            positive_samples,
+            negative_images_dir,
+            model_filename,
+            input_shape,
+            num_classes,
+            epochs,
+            model_saving_path,
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
